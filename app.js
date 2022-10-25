07926a20b63e2bc714bdf18683a593b37037b199
Mario Arce <elmariobros96@gmail.com>
2022-10-24 19:58:44 -0700

    Se agrego nuevos divs, un generador dinamico, un header responsivo, una imagen de logo ejemplo, etc

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const btn1 = document.querySelector('#boton1');
-const parrafo1 = document.querySelector('.texto1');
-const parrafo2 = document.querySelector('.texto2');
+const datos = [{
+    img: '',
+    text: "Hollow Knight es el juego que revoluciono los metroidvanias, así como su gameplay"
+}
+];
 
+let contenidoPrincipal = document.querySelector('#contenido-principal');
 
-btn1.addEventListener('click', function(){
+function contenidoCuadrosIzq (text) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    //El contenedor principal adopta al nuevo div
+    contenidoPrincipal.appendChild(div);
+    //Se le carga el contenido del texto al nuevo parrafo
+    p.innerHTML = text;
+    //El nuevo div adopta al nuevo parrafo
+    div.appendChild(p);
+    //Se agrega los atributos del div del cuadro izquierda al nuvo div
+    div.setAttribute('id','cuadro-1-iz');
+}
 
-parrafo1.innerText = 'Hola, Peter';
-parrafo2.style.background = 'red';
-parrafo2.innerText = 'Hola, Dalek';
-
-});
+/* Para cada dato, voy a pasar el texto al nuevo p de cada div*/
+datos.forEach(({text}) => contenidoCuadrosIzq(text));
